274bcba8d74e24c46b69543a58919c9e364e9c7e
CodigoAzulTec2 <codigoazulolimpiadas@gmail.com>
2026-08-31 14:42:49 -0300

    fix: correct apache conf typo in startup script

--- docker-apache-start.sh
+++ docker-apache-start.sh
@@ -6,6 +6,6 @@ PORT="${PORT:-80}"
 
 # Reconfigurar Apache para escuchar en el puerto dinámico
 sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/\\*:80/\\*:${PORT}/g" /etc/apache2/sites-available/0000-default.conf
+sed -i "s/\\*:80/\\*:${PORT}/g" /etc/apache2/sites-available/000-default.conf
 
 exec apache2-foreground
